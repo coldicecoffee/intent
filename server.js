0f628caeb5f98b7b2949a4bc64aaf51f8f5db1d7
Jude Woo <kw021992@gmaill.com>
2019-12-18 17:27:53 +0900

    push

---
+++ server.js
@@ -0,0 +1,121 @@
+const express = require("express");
+const app = express();
+
+const items = [
+  {
+    id: "A",
+    description: "Apple",
+    unit_price: 2.0,
+    volume_discounts: [{ number: 4, price: 7.0 }]
+  },
+  {
+    id: "B",
+    description: "Banana",
+    unit_price: 12.0,
+    volume_discounts: []
+  },
+  {
+    id: "C",
+    description: "Cranberry",
+    unit_price: 1.25,
+    volume_discounts: [{ number: 6, price: 6.0 }]
+  },
+  {
+    id: "D",
+    description: "Durian",
+    unit_price: 0.15,
+    volume_discounts: []
+  }
+];
+
+let cart = {
+  list: [], // [A, B, C]
+  counter: {}, // {A: 1, B: 1, C: 1}
+  total_by_item: {}, // {A: 2.0, C: 1.25}
+  total: 0
+};
+
+function addItem(id) {
+  "use strict";
+
+  for (const item of items) {
+    if (id === item.id) {
+      const uPrice = item.unit_price;
+
+      cart.list.push(id);
+
+      if (id in cart.counter) {
+        cart.counter[id] += 1;
+      } else {
+        cart.counter[id] = 1;
+      }
+      // console.log("Counter: " + JSON.stringify(cart.counter));
+
+      if (id === "A" || id === "C") {
+        const vDNum = item.volume_discounts[0].number;
+        const vDPrice = item.volume_discounts[0].price;
+        const vCount = Math.floor(cart.counter[id] / vDNum);
+        const uCount = cart.counter[id] % vDNum;
+        const prev = id in cart.total_by_item ? cart.total_by_item[id] : 0;
+        // console.log("volumn_discount number: " + vDNum);
+        // console.log("volumn_discount price: " + vDPrice);
+        // console.log("volumn_discount count: " + vCount);
+        // console.log("unit count: " + uCount);
+        cart.total_by_item[id] = vCount * vDPrice + uPrice * uCount;
+        cart.total += cart.total_by_item[id] - prev;
+      } else {
+        cart.total += uPrice * 1;
+      }
+
+      // console.log("Total: " + cart.total);
+      return;
+    }
+  }
+}
+
+function findItem(id) {
+  "use strict";
+
+  for (const item of items) {
+    if (id === item.id) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+app.get("/initCart", function(req, res) {
+  cart.list = [];
+  cart.counter = {};
+  cart.total_by_item = {};
+  cart.total = 0;
+  res.json({ message: "Starting a new shopping-cart" });
+});
+
+app.post("/addItems/:id", function(req, res) {
+  const id = req.params.id;
+  if (findItem(id)) {
+    addItem(id);
+    res.json({
+      message: "Item '" + id + "' has been added to the shopping-cart."
+    });
+  } else {
+    res.status(404).json("Item '" + id + "' not found.");
+  }
+});
+
+app.get("/retrieveTotal", function(req, res) {
+  res.json({
+    message: "The total is: " + cart.total,
+    total: cart.total
+  });
+});
+
+const server = app.listen(8888, function() {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log("Server listening at http://%s:%s", host, port);
+});
+
+module.exports = app;
---
+++ test/test.js
@@ -0,0 +1,80 @@
+//Require the dev-dependencies
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../server");
+let should = chai.should();
+
+chai.use(chaiHttp);
+describe("Shopping Cart Tests", () => {
+  beforeEach(done => {
+    chai
+      .request(server)
+      .get("/initCart")
+      .end((err, res) => {
+        done();
+      });
+  });
+  describe("Add 'ABCDABAA' to the cart", () => {
+    it("it should get a total of $32.40", done => {
+      let ids = "ABCDABAA";
+      for (const id of ids) {
+        chai
+          .request(server)
+          .post("/addItems/" + id)
+          .end((err, res) => {
+            res.should.have.status(200);
+          });
+      }
+      chai
+        .request(server)
+        .get("/retrieveTotal")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property("total").eql(32.4);
+          done();
+        });
+    });
+  });
+  describe("Add 'CCCCCCC' to the cart", () => {
+    it("it should get a total of $7.25", done => {
+      let ids = "CCCCCCC";
+      for (const id of ids) {
+        chai
+          .request(server)
+          .post("/addItems/" + id)
+          .end((err, res) => {
+            res.should.have.status(200);
+          });
+      }
+      chai
+        .request(server)
+        .get("/retrieveTotal")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property("total").eql(7.25);
+          done();
+        });
+    });
+  });
+  describe("Add 'ABCD' to the cart", () => {
+    it("it should get a total of $15.40", done => {
+      let ids = "ABCD";
+      for (const id of ids) {
+        chai
+          .request(server)
+          .post("/addItems/" + id)
+          .end((err, res) => {
+            res.should.have.status(200);
+          });
+      }
+      chai
+        .request(server)
+        .get("/retrieveTotal")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property("total").eql(15.4);
+          done();
+        });
+    });
+  });
+});
